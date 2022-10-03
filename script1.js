5ca7ee720b7599c65171ff5256545ecc12de9496
Claire_Gao <gaoyinlu007@outlook.com>
2022-10-03 14:09:12 +0200

    onClick event added

---
+++ script1.js
@@ -0,0 +1,35 @@
+// function myFunction(x) {
+    // x.classList.toggle("fa-thumbs-down");
+//   }
+  
+var clicks=0;
+var clicks1 = 0;
+var clicks2 = 0;
+var clicks3 = 0;
+var clicks4 = 0;
+
+
+
+function hello() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+function hello1() {
+        clicks1 += 1;
+        document.getElementById("clicks1").innerHTML = clicks1;
+    };
+
+function hello2() {
+        clicks2 += 1;
+        document.getElementById("clicks2").innerHTML = clicks2;
+    };
+
+function hello3() {
+        clicks3 += 1;
+        document.getElementById("clicks3").innerHTML = clicks3;
+    };
+function hello4() {
+        clicks4 += 1;
+        document.getElementById("clicks4").innerHTML = clicks4;
+    };
